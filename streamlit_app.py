fa51d7a94d4dcdf5958be2961b78551bfd5c6472
taka-oyafuso-lesson <148530243+taka-oyafuso-lesson@users.noreply.github.com>
2023-10-20 17:42:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
