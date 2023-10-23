c7fb66e1f523fdd78e6ad4cc5f41944ed0579aaa
taka-oyafuso-lesson <148530243+taka-oyafuso-lesson@users.noreply.github.com>
2023-10-23 10:02:54 +0900

    Update streamlit_app.py
    顧客が選択する果物に基づいてテーブル データをフィルタリングしたいので、顧客に例を示すためにリストを事前に設定します。

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # ここに選択リストを置き、含めたい果物を選択できるようにしましょう。
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# 顧客が選択する果物に基づいてテーブル データをフィルタリングしたいので、顧客に例を示すためにリストを事前に設定します。 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # ページにテーブルを表示します。
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+
