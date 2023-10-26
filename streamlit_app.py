4f6b645e3bd144d2ba7cbda1ce7f5300a86c86ac
taka-oyafuso-lesson <148530243+taka-oyafuso-lesson@users.noreply.github.com>
2023-10-26 11:35:51 +0900

    Update streamlit_app.py
    スペース追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,4 +68,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit'
 streamlit.write('Thanks for adding jackfruit', add_my_fruit)
 
 #this will not work correctly, but just to with it for now
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
