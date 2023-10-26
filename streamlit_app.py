c831980b249794947b331f0e91fabbbea97e1190
taka-oyafuso-lesson <148530243+taka-oyafuso-lesson@users.noreply.github.com>
2023-10-26 15:14:47 +0900

    Update streamlit_app.py
    stopの場所修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,9 +152,6 @@ try:
       streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
-        
-# don't run anything past here while we troubleshoot
-# streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
@@ -169,6 +166,8 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 #テキスト入力ボックスを追加
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit')
