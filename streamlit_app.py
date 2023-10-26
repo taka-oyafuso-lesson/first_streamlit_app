baa71235bde722f68707b9e057a3866d18f948d2
taka-oyafuso-lesson <148530243+taka-oyafuso-lesson@users.noreply.github.com>
2023-10-26 11:23:45 +0900

    Update streamlit_app.py
    テキスト入力ボックスを追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,3 +61,8 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#テキスト入力ボックスを追加
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('Thanks for adding jackfruit', add_my_fruit)
